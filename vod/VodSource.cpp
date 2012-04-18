0835f6a5e4850499eed8f4f1b5fbea1a8c73a8a7
zenzhang <zenzhang@pptv.com>
2012-04-18 05:49:49 +0000

    新增VodSource 用于点播播放
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@3272 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ vod/VodSource.cpp
@@ -0,0 +1,506 @@
+// VodSource.h
+
+#include "ppbox/demux/Common.h"
+#include "ppbox/demux/vod/VodSource.h"
+#include "ppbox/demux/pptv/PptvJump.h"
+#include "ppbox/demux/pptv/PptvDrag.h"
+#include "ppbox/demux/base/DemuxerError.h"
+
+#include <util/protocol/pptv/Url.h>
+#include <util/protocol/pptv/TimeKey.h>
+#include <util/archive/XmlIArchive.h>
+#include <util/archive/ArchiveBuffer.h>
+#include <util/buffers/BufferCopy.h>
+
+#include <framework/string/Parse.h>
+#include <framework/string/StringToken.h>
+#include <framework/string/Algorithm.h>
+#include <framework/timer/TimeCounter.h>
+#include <framework/logger/LoggerStreamRecord.h>
+#include <framework/network/NetName.h>
+using namespace framework::system::logic_error;
+using namespace framework::string;
+using namespace framework::logger;
+
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/condition_variable.hpp>
+using namespace boost::system;
+using namespace boost::asio;
+using namespace boost::asio::error;
+
+FRAMEWORK_LOGGER_DECLARE_MODULE_LEVEL("VodSource", 0);
+
+#ifndef PPBOX_DNS_VOD_JUMP
+#define PPBOX_DNS_VOD_JUMP "(tcp)(v4)jump.150hi.com:80"
+#endif
+
+#ifndef PPBOX_DNS_VOD_DRAG
+#define PPBOX_DNS_VOD_DRAG "(tcp)(v4)drag.150hi.com:80"
+#endif
+
+namespace ppbox
+{
+    namespace demux
+    {
+        static const framework::network::NetName dns_vod_jump_server(PPBOX_DNS_VOD_JUMP);
+        static const framework::network::NetName dns_vod_drag_server(PPBOX_DNS_VOD_DRAG);
+
+        static inline std::string addr_host(
+            framework::network::NetName const & addr)
+        {
+            return addr.host() + ":" + addr.svc();
+        }
+
+        VodSource::VodSource(
+            boost::asio::io_service & io_svc)
+            : HttpSource(io_svc)
+            , video_(NULL)
+            , jump_(new PptvJump(io_svc, JumpType::vod))
+            , drag_(new PptvDrag(io_svc))
+            , open_step_(StepType::not_open)
+            , bwtype_(0)
+            , vod_port_(2)
+            , url_("http://localhost/")
+            , max_dl_speed_(boost::uint32_t(-1))
+            , first_seg_(true)
+        {
+        }
+
+        VodSource::~VodSource()
+        {
+            if (jump_) {
+                delete jump_;
+                jump_ = NULL;
+            }
+            if (video_) {
+                delete video_;
+                video_ = NULL;
+            }
+        }
+
+        void VodSource::async_open(SourceBase::response_type const &resp)
+        {
+            resp_ = resp;
+            open_step_ = StepType::opening;
+            handle_async_open(boost::system::error_code());
+        }
+
+        framework::string::Url  VodSource::get_jump_url() const
+        {
+            framework::string::Url url("http://localhost/");
+            url.host(dns_vod_jump_server.host());
+            url.svc(dns_vod_jump_server.svc());
+            url.path("/" + name_ + "dt");
+
+            return url;
+        }
+        framework::string::Url  VodSource::get_drag_url() const
+        {
+            framework::string::Url url("http://localhost/");
+            url.host(dns_vod_drag_server.host());
+            url.svc(dns_vod_drag_server.svc());
+            url.path("/" + name_ + "0drag");
+            url.param("type", "ppbox");
+            return url;
+        }
+
+        void VodSource::parse_drag(
+            VodDragInfoNew & drag_info, 
+            boost::asio::streambuf & buf, 
+            boost::system::error_code const & ec)
+        {
+            if (ec) {
+                drag_info.ec = ec;
+                drag_info.is_ready = true;
+                return;
+            }
+
+            std::string buffer = boost::asio::buffer_cast<char const *>(buf.data());
+            LOG_S(Logger::kLevelDebug2, "[parse_drag] drag buffer: " << buffer);
+
+            boost::asio::streambuf buf2;
+            util::buffers::buffer_copy(buf2.prepare(buf.size()), buf.data());
+            buf2.commit(buf.size());
+
+            util::archive::XmlIArchive<> ia(buf2);
+            ia >> drag_info;
+            if (!ia) {
+                util::archive::XmlIArchive<> ia2(buf);
+                VodDragInfo drag_info_old;
+                ia2 >> drag_info_old;
+                if (!ia2) {
+                    drag_info.ec = error::bad_file_format;
+                } else {
+                    drag_info = drag_info_old;
+                }
+            }
+        }
+
+        void VodSource::parse_jump(
+            VodJumpInfoNoDrag & jump_info, 
+            boost::asio::streambuf & buf, 
+            boost::system::error_code & ec)
+        {
+            std::string buffer = boost::asio::buffer_cast<char const *>(buf.data());
+            LOG_S(Logger::kLevelDebug2, "[handle_jump] jump buffer: " << buffer);
+
+            boost::asio::streambuf buf2;
+            util::buffers::buffer_copy(
+                buf2.prepare(buf.size()), 
+                buf.data());
+            buf2.commit(buf.size());
+
+            util::archive::XmlIArchive<> ia(buf2);
+            ia >> (VodJumpInfo &)jump_info;
+            if (!ia) {
+                util::archive::XmlIArchive<> ia2(buf);
+                ia2 >> jump_info;
+                if (!ia2) {
+                    ec = error::bad_file_format;
+                }
+            }
+        }
+
+        std::string VodSource::get_key() const
+        {
+            return util::protocol::pptv::gen_key_from_time(server_time_ + (Time::now() - local_time_).total_seconds());
+        }
+
+        void VodSource::set_info_by_jump(
+            VodJumpInfoNoDrag & jump_info)
+        {
+            set_info_by_video(jump_info.video);
+            server_host_ = jump_info.server_host;
+            bwtype_ = jump_info.BWType;
+            server_time_ = jump_info.server_time.to_time_t();
+            local_time_ = Time::now();
+
+            assert(segments_.size() == 0);
+            if (jump_info.block_size != 0)
+            {
+                segments_.push_back(jump_info.firstseg);
+            }
+        }
+
+        void VodSource::set_info_by_drag(
+            VodDragInfoNew & drag_info)
+        {
+            set_info_by_video(drag_info.video);
+
+            std::vector<VodSegmentNew> & segmentsTmp = drag_info.segments;
+            for (size_t i = 0;  i < segmentsTmp.size(); ++i) {
+                if (i == 0 && segments_.size() > 0)
+                    continue;
+                segments_.push_back(segmentsTmp[i]);
+            }
+        }
+
+        void VodSource::set_info_by_video(
+            VodVideo & video)
+        {
+            if (NULL == video_)
+            {
+                video_ = new VodVideo(video);
+            }
+            else
+            {
+                *video_ = video;
+            }
+        }
+
+
+        void VodSource::handle_async_open(
+            boost::system::error_code const & ecc)
+        {
+            error_code ec = ecc;
+            if (ec) 
+            {
+                if (ec != boost::asio::error::would_block) 
+                {
+                    if (open_step_ == StepType::jump) 
+                    {
+                        LOG_S(Logger::kLevelAlarm, "jump: failure");
+                        response(ec);
+                    } 
+                    else if (open_step_ == StepType::drag) 
+                    {
+                        LOG_S(Logger::kLevelAlarm, "data: failure");
+                    }
+                }
+                return;
+            }
+
+            switch (open_step_) 
+            {
+            case StepType::opening:
+                {
+
+                    if (!ec) 
+                    {
+                        open_step_ = StepType::jump;
+
+                        LOG_S(Logger::kLevelEvent, "jump: start");
+
+                        jump_->async_get(
+                            get_jump_url(), 
+                            boost::bind(&VodSource::handle_async_open, this, _1));
+                        return;
+                    }
+                    else 
+                    {
+                        LOG_S(Logger::kLevelDebug, "url ec: " << ec.message());
+                    }
+                    break;
+                }
+            case StepType::jump:
+                {
+                    VodJumpInfoNoDrag jump_info;
+                    parse_jump(jump_info, jump_->get_buf(), ec);
+                    if (ec)
+                    {
+                        break;
+                    }
+
+                    set_info_by_jump(jump_info);
+
+                    if (segments_.size() > 0 && jump_info.video.duration == jump_info.firstseg.duration)
+                    {
+                        open_step_ = StepType::finish;
+
+                    }
+                    else
+                    {
+                        open_step_ = StepType::drag;
+                        drag_->async_get(
+                            get_drag_url(), 
+                            jump_info.server_host, 
+                            boost::bind(&VodSource::handle_async_open, this, _1));
+                    }
+                    break;
+                }
+            case StepType::drag:
+                {
+                    open_step_ = StepType::finish;
+                    VodDragInfoNew drag_info;
+                    parse_drag(drag_info,drag_->get_buf(),ec); //判断drag是否成功  / 是否获取到drag信息
+                    if (!ec)
+                    {
+                        set_info_by_drag(drag_info);
+                    }
+                    return;
+                }
+
+            default:
+                assert(0);
+                return;
+            }
+
+            response(ec);
+        }
+
+
+        void VodSource::response(
+            boost::system::error_code const & ec)
+        {
+            SourceBase::response_type resp;
+            resp.swap(resp_);
+
+            ios_service().post(boost::bind(resp, ec));
+        }
+
+
+        bool VodSource::is_open()
+        {
+            return (open_step_ > StepType::status);
+        }
+
+        boost::system::error_code VodSource::get_request(
+            size_t segment, 
+            boost::uint64_t & beg, 
+            boost::uint64_t & end, 
+            framework::network::NetName & addr, 
+            util::protocol::HttpRequest & request, 
+            error_code & ec)
+        {
+            util::protocol::HttpRequestHead & head = request.head();
+            ec = error_code();
+            if (segment < segments_.size()) {
+                if (vod_port_ 
+                    && buffer()->num_try() > 3) {
+                        buffer()->set_total_req(1);
+                        first_seg_ = true;
+                        vod_port_ = 0;
+                }
+                if (first_seg_) {
+                    first_seg_ = false;
+                    url_.host(server_host_.host());
+                    url_.svc(server_host_.svc());
+                    if (vod_port_) {
+                        set_time_out(0, ec);
+                        addr.host("127.0.0.1");
+                        addr.port(vod_port_);
+                        head.host.reset(addr_host(addr));
+                    } else {
+                        set_time_out(5 * 1000, ec);
+                        if (proxy_addr_.host().empty()) {
+                            addr = server_host_;
+                        } else {
+                            addr = proxy_addr_;
+                        }
+                        head.host.reset(addr_host(server_host_));
+                        if (head.pragma.empty())
+                            head.pragma.push_back("Client=PPLiveVA/1,5,2,1");
+                    }
+                }
+                url_.path("/" + format(segment) + "/" + name_);
+                url_.param("key", get_key());
+                if (vod_port_) {
+                    Url url("http://localhost/ppvaplaybyopen?");
+                    url.param("url", url_.to_string());
+                    url.param("rid", segments_[segment].va_rid);
+                    url.param("blocksize", format(segments_[segment].block_size));
+                    url.param("filelength", format(segments_[segment].file_length));
+                    url.param("headlength", format(segments_[segment].head_length));
+                    url.param("autoclose", "false");
+                    boost::uint32_t buf_time = vod_demuxer_->buf_time();
+                    url.param("drag", buf_time < 15000 ? "1" : "0");
+                    url.param("headonly", end <= segments_[segment].head_length ? "1" : "0");
+                    url.param("BWType", format(bwtype_));
+
+                    if(max_dl_speed_ != boost::uint32_t(-1))
+                        url.param("speedlimit", format(max_dl_speed_));
+                    url.param("blocknum", format(segments_[segment].block_num));
+
+                    url.encode();
+                    head.path = url.path_all();
+                } else {
+                    head.path = url_.path_all();
+                }
+
+                LOG_S(framework::logger::Logger::kLevelDebug, "Segment url: " << url_.to_string());
+            } else {
+                ec = item_not_exist;
+            }
+            return ec;
+        }
+
+        DemuxerType::Enum VodSource::demuxer_type() const
+        {
+            return DemuxerType::mp4;
+        }
+
+        void VodSource::set_url(std::string const &url)
+        {
+            boost::system::error_code ec;
+            //name_ = name;
+            std::string::size_type slash = url.find('|');
+
+            if (slash == std::string::npos) 
+            {
+                return;
+            } 
+            std::string key = url.substr(0, slash);
+            std::string playlink = url.substr(slash + 1);
+
+            //std::string   temp_host = "http://host/";
+            //url = temp_host + url;
+            playlink = framework::string::Url::decode(playlink);
+            framework::string::Url request_url(playlink);
+            playlink = request_url.path().substr(1);
+            std::string strBwtype = request_url.param("bwtype");
+
+            if(!strBwtype.empty())
+            {
+                bwtype_ = framework::string::parse<boost::int32_t>(strBwtype);
+            }
+
+            if (playlink.size() > 4 && playlink.substr(playlink.size() - 4) == ".mp4") {
+                if (playlink.find('%') == std::string::npos) {
+                    playlink = Url::encode(playlink, ".");
+                }
+            } else {
+                playlink = util::protocol::pptv::url_decode(playlink, key);
+                StringToken st(playlink, "||");
+                if (!st.next_token(ec)) {
+                    playlink = st.remain();
+                }
+            }
+            name_ = playlink;
+        }
+
+        boost::system::error_code VodSource::reset(size_t& segment)
+        {
+            segment = 0;
+            return boost::system::error_code();
+        }
+
+        boost::uint32_t VodSource::get_duration()
+        {
+            if (NULL != video_)
+            {
+                return video_->duration;
+            }
+            else
+            {
+                return boost::uint32_t(-1);
+            }
+        }
+
+        size_t VodSource::segment_count() const
+        {
+            size_t ret = size_t(-1);
+            if (segments_.size() > 0 &&  segments_[0].duration != video_->duration)
+            {
+                ret = segments_.size();
+            }
+            return ret;		
+        }
+
+        boost::uint64_t VodSource::segment_size(size_t segment)
+        {
+            boost::uint64_t ret = boost::uint64_t(-1);
+            if (segments_.size() > segment )
+            {
+                ret = segments_[segment].file_length;
+            }
+            return ret;
+        }
+
+        boost::uint64_t VodSource::segment_time(size_t segment)
+        {
+            boost::uint64_t ret = boost::uint64_t(-1);
+            if (segments_.size() > segment )
+            {
+                ret = segments_[segment].duration;
+            }
+            return ret;
+        }
+
+        void VodSource::update_segment_duration(size_t segment,boost::uint32_t time)
+        {
+            if (segments_.size() > segment )
+            {
+                segments_[segment].duration = time;
+            }
+        }
+
+        void VodSource::update_segment_file_size(size_t segment,boost::uint64_t fsize)
+        {
+            if (segments_.size() > segment )
+            {
+                segments_[segment].file_length = fsize;
+            }
+        }
+
+        void VodSource::update_segment_head_size(size_t segment,boost::uint64_t hsize)
+        {
+            if (segments_.size() > segment )
+            {
+                segments_[segment].head_length = hsize;
+            }
+        }
+
+    } // namespace demux
+} // namespace ppbox
---
+++ vod/VodSource.h
@@ -0,0 +1,139 @@
+// VodSource.h
+
+#ifndef _PPBOX_DEMUX_VOD_SOURCE_H_
+#define _PPBOX_DEMUX_VOD_SOURCE_H_
+
+#include "ppbox/demux/source/HttpSource.h"
+#include "ppbox/demux/vod/VodDemuxer.h"
+#include "ppbox/demux/vod/VodInfo.h"
+
+
+
+namespace ppbox
+{
+    namespace demux
+    {
+        struct VodJumpInfo;
+        struct VodJumpInfoNoDrag;
+        struct VodDragInfoNew;
+        struct VodVideo;
+
+        class VodSegmentDemuxer;
+        class VodSegments;
+        class PptvJump;
+        class PptvDrag;
+
+        class VodSource
+            : public HttpSource
+        {
+        public:
+            VodSource(
+                boost::asio::io_service & io_svc);
+
+            virtual ~VodSource();
+
+        public:
+            virtual void async_open(SourceBase::response_type const &resp);
+
+            virtual bool is_open();
+
+        public:
+            virtual DemuxerType::Enum demuxer_type() const;
+
+            virtual void set_url(std::string const &url);
+
+            virtual boost::system::error_code reset(size_t& segment);
+
+            virtual boost::uint32_t get_duration();
+
+            virtual void update_segment_duration(size_t segment,boost::uint32_t time);
+            virtual void update_segment_file_size(size_t segment,boost::uint64_t fsize);
+            virtual void update_segment_head_size(size_t segment,boost::uint64_t hsize);
+        private:
+            virtual size_t segment_count() const;
+            virtual boost::uint64_t segment_size(size_t segment);
+            virtual boost::uint64_t segment_time(size_t segment);
+
+        private:
+            virtual boost::system::error_code get_request(
+                size_t segment, 
+                boost::uint64_t & beg, 
+                boost::uint64_t & end, 
+                framework::network::NetName & addr, 
+                util::protocol::HttpRequest & request, 
+                boost::system::error_code & ec);
+
+            void handle_async_open(
+                boost::system::error_code const & ecc);
+
+            void response(
+                boost::system::error_code const & ec);
+
+            framework::string::Url get_jump_url() const;
+            framework::string::Url get_drag_url() const;
+
+            void parse_drag(
+                VodDragInfoNew & drag_info, 
+                boost::asio::streambuf & buf, 
+                boost::system::error_code const & ec);
+
+            void parse_jump(
+                VodJumpInfoNoDrag & jump_info, 
+                boost::asio::streambuf & buf, 
+                boost::system::error_code & ec);
+
+
+            void set_info_by_jump(
+                VodJumpInfoNoDrag & jump_info);
+
+            void set_info_by_video(
+                VodVideo & video);
+
+            void set_info_by_drag(
+                VodDragInfoNew & drag_info);
+
+            std::string get_key() const;
+
+        private:
+            struct StepType
+            {
+                enum Enum
+                {
+                    not_open, 
+                    opening, 
+                    jump,
+                    status,
+                    drag, 
+                    finish, 
+                    finish2
+                };
+            };
+            //Demux
+        private:
+            VodVideo * video_;
+            PptvJump * jump_;
+            boost::shared_ptr<PptvDrag> drag_;
+            SourceBase::response_type resp_;
+
+            StepType::Enum open_step_;
+            //vod segment
+        private:
+            boost::uint16_t vod_port_;
+            std::string name_;
+            framework::network::NetName server_host_;
+            framework::network::NetName proxy_addr_;
+            boost::int32_t bwtype_;
+            bool first_seg_;
+
+            Time local_time_;
+            time_t server_time_;
+            Url url_;
+            std::vector<VodSegmentNew> segments_;
+            boost::uint32_t max_dl_speed_;
+            VodDemuxer * vod_demuxer_;
+        };
+
+    } // namespace demux
+} // namespace ppbox
+
+#endif // _PPBOX_DEMUX_VOD_SOURCE__H_
