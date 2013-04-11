7508e71c676bbd5c4f70ca425c60c471ee0f6041
isxxguo <isxxguo@pptv.com>
2013-04-11 09:26:49 +0000

    /ppbox/branches/1.2.0 /src/ppbox 修正细节问题 1、修正 IDemuxer.cpp Cache 构造部分成员没有初始化问题 2、修正 demux FlvStream 处理 mp3 时 format_data 设置问题 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6594 b3a1389e-2159-5c43-9038-463bf877d0d9

--- basic/flv/FlvStream.h
+++ basic/flv/FlvStream.h
@@ -47,6 +47,11 @@ namespace ppbox
                 if (DataSize > 0) {
                     if (FlvTagType::VIDEO == Type) { 
                         type = MEDIA_TYPE_VIDE;
+                        time_scale = 1000;
+                        video_format.frame_rate = 0;
+                        video_format.width = 0;
+                        video_format.height = 0;
+                        format_data = codec_data;
 
                         switch (VideoHeader.CodecID) {
                             case FlvVideoCodec::H264:
@@ -58,13 +63,12 @@ namespace ppbox
                                 format_type = 0;
                                 break;
                         }
-                        video_format.frame_rate = 0;
-                        video_format.width = 0;
-                        video_format.height = 0;
+
                         if (sub_type == VIDEO_TYPE_AVC1) {
                             ppbox::avformat::AvcConfigHelper ac(codec_data);
                             ac.get_format(video_format);
                         }
+
                         if (metadata.framerate) {
                             video_format.frame_rate = metadata.framerate;
                         }
@@ -74,10 +78,17 @@ namespace ppbox
                         if (metadata.height) {
                             video_format.height = metadata.height;
                         }
-                        time_scale = 1000;
-                        format_data = codec_data;
                     } else if (FlvTagType::AUDIO == Type) {
                         type = MEDIA_TYPE_AUDI;
+                        time_scale = 1000;
+                        boost::uint32_t frequency[4] = {5500, 11025, 22050, 44100};
+                        boost::uint32_t size[2] = {8, 16};
+                        boost::uint32_t channel[2] = {1, 2};
+                        audio_format.sample_rate = frequency[AudioHeader.SoundRate];
+                        audio_format.sample_size = size[AudioHeader.SoundSize];
+                        audio_format.channel_count = channel[AudioHeader.SoundType];
+                        format_data = codec_data;
+
                         switch (AudioHeader.SoundFormat) {
                             case FlvSoundCodec::AAC:
                                 sub_type = AUDIO_TYPE_MP4A;
@@ -86,29 +97,24 @@ namespace ppbox
                             case FlvSoundCodec::MP3:
                                 sub_type = AUDIO_TYPE_MP1A;
                                 format_type = StreamInfo::audio_iso_mp4;
+                                format_data.clear();
                                 break;
                             default:
                                 sub_type = AUDIO_TYPE_NONE;
                                 format_type = 0;
                                 break;
                         }
-                        time_scale = 1000;
-                        boost::uint32_t frequency[4] = {5500, 11025, 22050, 44100};
-                        boost::uint32_t size[2] = {8, 16};
-                        boost::uint32_t channel[2] = {1, 2};
-                        audio_format.sample_rate = frequency[AudioHeader.SoundRate];
-                        audio_format.sample_size = size[AudioHeader.SoundSize];
-                        audio_format.channel_count = channel[AudioHeader.SoundType];
+
                         if (sub_type == AUDIO_TYPE_MP4A) {
                             ppbox::avformat::AacConfigHelper ac(codec_data);
                             audio_format.sample_rate = ac.get_frequency();
                             audio_format.channel_count = ac.get_channel_count();
                         }
+
                         if (metadata.audiosamplerate > 0)
                             audio_format.sample_rate = metadata.audiosamplerate;
                         if (metadata.audiosamplesize > 0)
                             audio_format.sample_size = metadata.audiosamplesize;
-                        format_data = codec_data;
                     }
                 }
             }
